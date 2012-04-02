f0be82a60f386807fa620dd8f7047b26315c36b1
Yuuki Furuyama <addsict@gmail.com>
2012-04-03 02:56:19 +0900

    コメントアウトされていた必要ないコードを削除

--- background.js
+++ background.js
@@ -1,27 +1,12 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  chrome.tabs.query({},function(results) {  
-    for (var i = 0, l = results.length; i < l; i++) {
-      if (results[i].url == "chrome://settings/extensions#") {
-        var tabId = results[i].id;
-        chrome.tabs.update(tabId, {active: true});
-      }
-    }
-  });
-});
 /*
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // query$B$K(B"chrome://settings/extensions#"$B$r(Burl$B$H$7$FF~$l$F$b(Bextension$B$N%Z!<%8$,<hF@$G$-$J$$$N$G!"(B
-  //  $BA4$F$N%?%V$r<hF@$7$F$+$i(Bextension$B$N%?%V$rC5$9(B
-  //
-  chrome.tabs.query({},function(results) {  
-    for (var i = 0, l = results.length; i < l; i++) {
-      if (results[i].url == "chrome://settings/extensions#") {
-        var tabId = results[i].id;
-        chrome.tabs.update(tabId, {active: true}, function(tab) {
-          reloadExtension("dimdcggmcokdnapmajgeikecmejcmmfe");
-        });
-      }
-    }
-  });
+ * background.js
+ */
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  var extensionId = request.id;
+  chrome.management.setEnabled(extensionId, false);
+  setTimeout(function() {
+    // 念のため500msにセット
+    chrome.management.setEnabled(extensionId, true);
+  }, 500);
 });
-*/
--- contentscript.js
+++ contentscript.js
@@ -1,21 +1,12 @@
+/*
+ * contentscript.js
+ */
+
 var KEY_CODE_r = 82;
-window.onkeydown = function(event) {
+var EXTENSION_ID = "dimdcggmcokdnapmajgeikecmejcmmfe";
+
+document.onkeydown = function(event) {
   if (event.keyCode == KEY_CODE_r && event.ctrlKey) {
-    chrome.extension.sendRequest({});
+    chrome.extension.sendRequest({id: EXTENSION_ID});
   }
 }
-
-// function reloadExtension(extensionId) {
-  // var event = document.createEvent("MouseEvents");
-  // event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-
-  // var links = document.getElementsByClassName("extension-links-trailing");
-  // alert(links.length);
-  // for (var i = 0, l = links.length; i < l; i++) {
-    // if (links[i].id === extensionId) {
-      // links[i].dispatchEvent(event);
-      // break;
-    // }
-  // }
-// }
-// reloadExtension("dimdcggmcokdnapmajgeikecmejcmmfe");
